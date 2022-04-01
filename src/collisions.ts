a783283eeddcf84312ae2831f5338007a88ac7ac
Eric Sexton <foopis23@gmail.com>
2022-04-01 01:32:20 -0400

    reformatted and version update

--- src/collisions.ts
+++ src/collisions.ts
@@ -1,14 +1,14 @@
-import { Vector2, Math } from 'simple-game-math'
+import { Vector2, Math } from 'simple-game-math';
 
 export function isCircleVsCircleCollision(
   pos1: Vector2.IVector2,
   pos2: Vector2.IVector2,
   radius1: number,
-  radius2: number
+  radius2: number,
 ): boolean {
-  const radiiSum = radius1 + radius2
-  const distance = Vector2.distance(pos1, pos2)
-  return distance < radiiSum
+  const radiiSum = radius1 + radius2;
+  const distance = Vector2.distance(pos1, pos2);
+  return distance < radiiSum;
 }
 
 /**
@@ -18,35 +18,35 @@ export function resolveCircleVsCircleCollision(
   pos1: Vector2.IVector2,
   pos2: Vector2.IVector2,
   radius1: number,
-  radius2: number
+  radius2: number,
 ): Vector2.IVector2 {
-  const distance = Vector2.distance(pos1, pos2)
-  const radiiSum = radius1 + radius2
-  const xDiff = pos2.x - pos1.x
-  const yDiff = pos2.y - pos1.y
-  const xUnit = xDiff / distance
-  const yUnit = yDiff / distance
+  const distance = Vector2.distance(pos1, pos2);
+  const radiiSum = radius1 + radius2;
+  const xDiff = pos2.x - pos1.x;
+  const yDiff = pos2.y - pos1.y;
+  const xUnit = xDiff / distance;
+  const yUnit = yDiff / distance;
 
   return {
     x: pos1.x + radiiSum * xUnit,
-    y: pos1.y + radiiSum * yUnit
-  }
+    y: pos1.y + radiiSum * yUnit,
+  };
 }
 
 export function isCircleVsRectangleCollision(
   circlePos: Vector2.IVector2,
   circleRadius: number,
   rectPos: Vector2.IVector2,
-  rectSize: Vector2.IVector2
+  rectSize: Vector2.IVector2,
 ): boolean {
   const nearestPoint = {
     x: Math.clamp(circlePos.x, rectPos.x, rectPos.x + rectSize.x),
-    y: Math.clamp(circlePos.y, rectPos.y, rectPos.y + rectSize.y)
-  }
-  const rayToNearest = Vector2.subtract(nearestPoint, circlePos)
-  const overlap = circleRadius - Vector2.mag(rayToNearest)
+    y: Math.clamp(circlePos.y, rectPos.y, rectPos.y + rectSize.y),
+  };
+  const rayToNearest = Vector2.subtract(nearestPoint, circlePos);
+  const overlap = circleRadius - Vector2.mag(rayToNearest);
 
-  return overlap > 0
+  return overlap > 0;
 }
 
 /**
@@ -56,21 +56,21 @@ export function resolveCircleVsRectangleCollision(
   circlePos: Vector2.IVector2,
   circleRadius: number,
   rectPos: Vector2.IVector2,
-  rectSize: Vector2.IVector2
+  rectSize: Vector2.IVector2,
 ): Vector2.IVector2 {
   const nearestPoint = {
     x: Math.clamp(circlePos.x, rectPos.x, rectPos.x + rectSize.x),
-    y: Math.clamp(circlePos.y, rectPos.y, rectPos.y + rectSize.y)
-  }
+    y: Math.clamp(circlePos.y, rectPos.y, rectPos.y + rectSize.y),
+  };
 
-  const rayToNearest = Vector2.subtract(nearestPoint, circlePos)
-  const overlap = circleRadius - Vector2.mag(rayToNearest)
+  const rayToNearest = Vector2.subtract(nearestPoint, circlePos);
+  const overlap = circleRadius - Vector2.mag(rayToNearest);
 
-  const temp = Vector2.normalize(rayToNearest)
-  temp.x = temp.x * overlap
-  temp.y = temp.y * overlap
+  const temp = Vector2.normalize(rayToNearest);
+  temp.x = temp.x * overlap;
+  temp.y = temp.y * overlap;
 
-  const finalPos = Vector2.subtract(circlePos, temp)
+  const finalPos = Vector2.subtract(circlePos, temp);
   return finalPos;
 }
 
@@ -78,20 +78,20 @@ export function resolveRectangleVsCircleCollision(
   rectPos: Vector2.IVector2,
   rectSize: Vector2.IVector2,
   circlePos: Vector2.IVector2,
-  circleRadius: number
+  circleRadius: number,
 ): Vector2.IVector2 {
   const nearestPoint = {
     x: Math.clamp(circlePos.x, rectPos.x, rectPos.x + rectSize.x),
-    y: Math.clamp(circlePos.y, rectPos.y, rectPos.y + rectSize.y)
-  }
+    y: Math.clamp(circlePos.y, rectPos.y, rectPos.y + rectSize.y),
+  };
 
-  const rayToNearest = Vector2.subtract(nearestPoint, rectPos)
-  const overlap = circleRadius - Vector2.mag(rayToNearest)
+  const rayToNearest = Vector2.subtract(nearestPoint, rectPos);
+  const overlap = circleRadius - Vector2.mag(rayToNearest);
 
-  const temp = Vector2.normalize(rayToNearest)
-  temp.x = temp.x * overlap
-  temp.y = temp.y * overlap
+  const temp = Vector2.normalize(rayToNearest);
+  temp.x = temp.x * overlap;
+  temp.y = temp.y * overlap;
 
-  const finalPos = Vector2.subtract(rectPos, temp)
+  const finalPos = Vector2.subtract(rectPos, temp);
   return finalPos;
 }
--- src/kinematics.ts
+++ src/kinematics.ts
@@ -1,35 +1,31 @@
-import { Vector2, Math } from "simple-game-math";
+import { Vector2, Math } from 'simple-game-math';
 
 export function applyAcceleration(
   velocity: Vector2.IVector2,
   acceleration: Vector2.IVector2,
-  deltaTime: number
+  deltaTime: number,
 ): Vector2.IVector2 {
   return {
     x: velocity.x + acceleration.x * deltaTime,
-    y: velocity.y + acceleration.y * deltaTime
-  }
+    y: velocity.y + acceleration.y * deltaTime,
+  };
 }
 
 export function applyVelocity(
   position: Vector2.IVector2,
   velocity: Vector2.IVector2,
-  deltaTime: number
+  deltaTime: number,
 ): Vector2.IVector2 {
   return {
     x: position.x + velocity.x * deltaTime,
-    y: position.y + velocity.y * deltaTime
-  }
+    y: position.y + velocity.y * deltaTime,
+  };
 }
 
-export function applyDrag(
-  velocity: Vector2.IVector2,
-  terminalVelocity: number,
-  deltaTime: number
-): Vector2.IVector2 {
-  const drag = Vector2.mag(velocity) / terminalVelocity
+export function applyDrag(velocity: Vector2.IVector2, terminalVelocity: number, deltaTime: number): Vector2.IVector2 {
+  const drag = Vector2.mag(velocity) / terminalVelocity;
   return {
     x: Math.moveTowards(velocity.x, 0, velocity.x * drag * deltaTime),
-    y: Math.moveTowards(velocity.y, 0, velocity.y * drag * deltaTime)
-  }
+    y: Math.moveTowards(velocity.y, 0, velocity.y * drag * deltaTime),
+  };
 }
