be144db7b1837546d9f897db3d36a9e8e7bad934
Eric Sexton <foopis23@gmail.com>
2022-04-01 01:24:24 -0400

    move some physics code over

---
+++ src/kinematics.ts
@@ -0,0 +1,35 @@
+import { Vector2, Math } from "simple-game-math";
+
+export function applyAcceleration(
+  velocity: Vector2.IVector2,
+  acceleration: Vector2.IVector2,
+  deltaTime: number
+): Vector2.IVector2 {
+  return {
+    x: velocity.x + acceleration.x * deltaTime,
+    y: velocity.y + acceleration.y * deltaTime
+  }
+}
+
+export function applyVelocity(
+  position: Vector2.IVector2,
+  velocity: Vector2.IVector2,
+  deltaTime: number
+): Vector2.IVector2 {
+  return {
+    x: position.x + velocity.x * deltaTime,
+    y: position.y + velocity.y * deltaTime
+  }
+}
+
+export function applyDrag(
+  velocity: Vector2.IVector2,
+  terminalVelocity: number,
+  deltaTime: number
+): Vector2.IVector2 {
+  const drag = Vector2.mag(velocity) / terminalVelocity
+  return {
+    x: Math.moveTowards(velocity.x, 0, velocity.x * drag * deltaTime),
+    y: Math.moveTowards(velocity.y, 0, velocity.y * drag * deltaTime)
+  }
+}
